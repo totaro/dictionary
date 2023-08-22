af42970234ad5721e8d446d79fa8a421c75cebcc
totaro <66010583+totaro@users.noreply.github.com>
2023-08-22 16:23:11 +0300

    new nav header

--- index.js
+++ index.js
@@ -82,3 +82,31 @@ inputEl.addEventListener("keyup", (e)=>{
             fetchAPI(e.target.value)
         }
 })
+
+
+/* Header */
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function(){
+
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
+
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.scrollY >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
